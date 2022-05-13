04a4eecc8aa6cebe9987c59fb327615ba2cdd542
Mahammad Mustafayev <tu7ekdm78@aztu.local>
2022-05-13 16:44:47 +0400

    added

--- Js-LocalStorage/assets/js/main.js
+++ Js-LocalStorage/assets/js/main.js
@@ -1,3 +1,16 @@
+let btn1=document.getElementById("maincard");
+let card=document.getElementById("cartModal");
+let del=document.getElementById("del");
+let trash=document.getElementById("trash");
+
+btn1.addEventListener("click",function () {
+    card.classList.toggle("d-none");
+})
+
+trash.addEventListener("click",function () {
+    trash.parentElement.parentElement.parentElement.remove();
+})
+
 let toBasketbtns = document.querySelectorAll(".ti-bag");
 
 toBasketbtns.forEach(btn => {
@@ -30,6 +43,8 @@ function getBasketItem(elem) {
   }
 }
 
+
+
 function fillBasket() {
   checkBasket();
   let basketItems = JSON.parse(localStorage.getItem("basket"));
@@ -39,18 +54,41 @@ function fillBasket() {
     item=>{
       itemCount += item.count;
       cardTableBody.innerHTML += 
-      `<tr data-id = ${item.id}>
-        <td class="w-25">
-          <img src="${item.img}" class="img-fluid img-thumbnail" alt="Sheep">
-        </td>
-        <td>${item.model}</td>
-        <td>${item.price}</td>
-        <td class="qty">${item.count}</td>
-        <td>${parseInt(item.price)*item.count}$</td>
-        <td>
-          <i class="fa fa-times"></i>
-        </td>
-      </tr>`
+      `<div class="col-md-2">
+      <img src="${item.img}"
+        class="img-fluid" alt="Generic placeholder image">
+    </div>
+    <div class="col-md-2 d-flex justify-content-center">
+      <div>
+        <p class="small text-muted mb-4 pb-2">Name</p>
+        <p class="lead fw-normal mb-0">${item.model}</p>
+      </div>
+    </div>
+    
+    <div class="col-md-2 d-flex justify-content-center">
+      <div>
+        <p class="small text-muted mb-4 pb-2">Quantity</p>
+        <p class="lead fw-normal mb-0">${item.count}</p>
+      </div>
+    </div>
+    <div class="col-md-2 d-flex justify-content-center">
+      <div>
+        <p class="small text-muted mb-4 pb-2">Price</p>
+        <p class="lead fw-normal mb-0">$${item.price}</p>
+      </div>
+    </div>
+    <div class="col-md-2 d-flex justify-content-center">
+      <div>
+        <p class="small text-muted mb-4 pb-2">Total</p>
+        <p class="lead fw-normal mb-0">${parseInt(item.price)*item.count}$</p>
+      </div>
+    </div>
+    <div class="col-md-2 d-flex justify-content-center">
+        <div>
+          <p id="del" class="small text-muted mb-4 pb-2">Delete</p>
+          <i id="trash" class="bi bi-trash3"></i>
+            
+        </div>`
     }
   )
   document.querySelector(".badge").innerText = itemCount;
@@ -64,4 +102,3 @@ function checkBasket() {
     localStorage.setItem("basket",JSON.stringify([]))
   }
 }
-
